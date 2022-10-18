0409a88fcada3b4dac2fff867abe0fb6f4fb98e5
foamcleaner <98933607+foamcleaner@users.noreply.github.com>
2022-10-18 08:37:05 +0300

    Add files via upload

--- form.php
+++ form.php
@@ -1,29 +1,25 @@
 <?php
-define("RECAPTCHA_V3_SECRET_KEY", '6LcD2YwiAAAAAGRmKJTrWnsMaUX-rtt8ASIH7XEk');
- 
- 
-$token = $_POST['token'];
-
- 
-// call curl to POST request
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL,"https://www.google.com/recaptcha/api/siteverify");
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('secret' => RECAPTCHA_V3_SECRET_KEY, 'response' => $token)));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-$response = curl_exec($ch);
-curl_close($ch);
-$arrResponse = json_decode($response, true);
- 
-// verify the response
-if($arrResponse["success"] == '1'  && $arrResponse["score"] >= 0.5) {
-    // valid submission
-    // go ahead and do necessary stuff
-	 echo '<h2>Thanks for posting comment</h2>';
-      header('Location: https://besttovarsale.com/page/583a8db650a1525fe57d1e876ad6b595e8e49561/');
+    $captcha;
+    
+    if(isset($_POST['g-recaptcha-response'])){
+      $captcha=$_POST['g-recaptcha-response'];
+    }
+    if(!$captcha){
+      echo '<h2>Please check the the captcha form.</h2>';
+      exit;
+    }
+    $secretKey = "6Le97owiAAAAANTGcnGqk227nBPs7W-xWudKT81Y";
+    $ip = $_SERVER['REMOTE_ADDR'];
+    // post request to server
+    $url = 'https://www.google.com/recaptcha/api/siteverify?secret=' . urlencode($secretKey) .  '&response=' . urlencode($captcha);
+    $response = file_get_contents($url);
+    $responseKeys = json_decode($response,true);
+    // should return JSON with success as true
+    if($responseKeys["success"]) {
+            echo '<h2>Thanks for posting comment</h2>';
+            header('Location: https://besttovarsale.com/page/583a8db650a1525fe57d1e876ad6b595e8e49561/');
             exit;
-} else {
-		echo '<h2>You are spammer ! Get the @$%K out</h2>';
-    // spam submission
-    // show error message
-}
+    } else {
+            echo '<h2>You are spammer ! Get the @$%K out</h2>';
+    }
+?>
